d0d13fe011e1e00a7e8bbdb381d067357a9729b4
Erwan H <67637529+ERHash@users.noreply.github.com>
2024-02-11 00:35:41 +0100

    Update nfctool.py
    help info available without card

--- nfctool.py
+++ nfctool.py
@@ -11,19 +11,6 @@ if len(sys.argv) < 2:
 	print("usage: nfcTool.py <command>\nList of available commands: help, mute, unmute, getuid, info, loadkey, read, firmver")
 	sys.exit()
 
-r = readers()
-if len(r) < 1:
-	print("error: No readers available!")
-	sys.exit()
-
-print("Available readers: ", r)
-
-reader = r[0]
-print ("Using: ", reader)
-
-connection = reader.createConnection()
-connection.connect()
-
 #detect command
 cmd = sys.argv[1]
 
@@ -40,6 +27,19 @@ if cmd == "help":
 	#print ("\tread [-s <sector>] [-h | -a] [-d | -t] \tRead sector <sector> (or all sectors) with loaded key. Print as [hex | ascii]. Print [data only | trailer only]")
 	print ("\tfirmver\tPrint the firmware version of the reader.")
 	sys.exit()
+
+r = readers()
+if len(r) < 1:
+	print("error: No readers available!")
+	sys.exit()
+
+print("Available readers: ", r)
+
+reader = r[0]
+print ("Using: ", reader)
+
+connection = reader.createConnection()
+connection.connect()
 
 cmdMap = {
 	"mute":[0xFF, 0x00, 0x52, 0x00, 0x00],
